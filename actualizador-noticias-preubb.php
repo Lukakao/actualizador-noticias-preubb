5f47f78ab3dc3f96a1df1e83c97e9298efa54afc
Lukakao <lukassanhuezasolar@gmail.com>
2024-11-01 16:16:55 -0300

    upload

---
+++ actualizador-noticias-preubb.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package ActualizadorNoticiasPreubb
+ */
+
+/*
+ Plugin Name: Actualizador Noticias Preubb
+ Plugin URI: 
+ Description: Sube las publicaciones de la pagina de facebook del preuniversitario 
+ Version: 1.0.0
+ Author: Lukas Sanhueza Solar
+ */
