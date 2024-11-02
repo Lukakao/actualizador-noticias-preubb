537d87200fa6cdda2e3afd8808226ed0ca99a4c9
Lukakao <lukassanhuezasolar@gmail.com>
2024-11-01 21:19:49 -0300

    testeo llamadas periodicas

--- actualizador-noticias-preubb.php
+++ actualizador-noticias-preubb.php
@@ -5,8 +5,50 @@
 
 /*
  Plugin Name: Actualizador Noticias Preubb
- Plugin URI: 
+ Plugin URI: https://github.com/Lukakao/actualizador-noticias-preubb
  Description: Sube las publicaciones de la pagina de facebook del preuniversitario 
  Version: 1.0.0
  Author: Lukas Sanhueza Solar
+ Author URI: https://github.com/Lukakao
+ License: GPLv2 or later
+ Text Domain: actualizador-noticias-preubb
+ License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
+
+ if (!defined('ABSPATH')) {
+    die("GUgu gaga no direct access chupalo");
+}
+
+add_filter('cron_schedules', 'custom_cron_schedule');
+
+function custom_cron_schedule($schedules) {
+    $schedules['five_seconds'] = array(
+        'interval' => 5,
+        'display' => __('Every 5 Seconds'),
+    );
+    return $schedules;
+}
+
+add_action('wp', 'programar_siguiente_checkeo');
+
+function programar_siguiente_checkeo() {
+    if (!wp_next_scheduled('checkeo_hook')) {
+        wp_schedule_event(time(), 'five_seconds', 'checkeo_hook');
+    }
+}
+
+function checkeo() {
+    error_log('poi'); 
+}
+
+add_action('checkeo_hook', 'checkeo');
+
+register_deactivation_hook(__FILE__, 'desactivar_checkeo');
+
+function desactivar_checkeo() {
+    $timestamp = wp_next_scheduled('checkeo_hook');
+    wp_unschedule_event($timestamp, 'checkeo_hook');
+    
+}
+
+
---
+++ index.php
@@ -0,0 +1,2 @@
+<?php
+# i wish i could watch myself watching a woman washing
