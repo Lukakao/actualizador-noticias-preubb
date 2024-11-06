b14bf9ae9dc39fd8cfa1b2860bf665b5a3cb0ac2
Lukakao <lukassanhuezasolar@gmail.com>
2024-11-06 14:31:28 -0300

    gugu

--- actualizador-noticias-preubb.php
+++ actualizador-noticias-preubb.php
@@ -15,39 +15,39 @@
  License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
 
- if (!defined('ABSPATH')) {
-    die("GUgu gaga no direct access chupalo");
+
+if (!defined('ABSPATH')) {
+die("no direct access");
 }
 
-add_filter('cron_schedules', 'custom_cron_schedule');
-
-function custom_cron_schedule($schedules) {
-    $schedules['five_seconds'] = array(
-        'interval' => 5,
-        'display' => __('Every 5 Seconds'),
-    );
-    return $schedules;
-}
-
-add_action('wp', 'programar_siguiente_checkeo');
-
+register_activation_hook(__FILE__, 'programar_siguiente_checkeo');
 function programar_siguiente_checkeo() {
     if (!wp_next_scheduled('checkeo_hook')) {
-        wp_schedule_event(time(), 'five_seconds', 'checkeo_hook');
+        wp_schedule_event(time(), 'everyhour', 'checkeo_hook');
     }
 }
 
+add_action('checkeo_hook', 'checkeo');
 function checkeo() {
     error_log('poi'); 
 }
 
-add_action('checkeo_hook', 'checkeo');
-
 register_deactivation_hook(__FILE__, 'desactivar_checkeo');
-
 function desactivar_checkeo() {
     $timestamp = wp_next_scheduled('checkeo_hook');
-    wp_unschedule_event($timestamp, 'checkeo_hook');
+    if ($timestamp) {
+        wp_unschedule_event($timestamp, 'checkeo_hook');
+    }
+}
+
+function actualizar_noticias(){
+    $base_url = "https://graph.facebook.com/v21.0/281060260937/feed";
+    $url = $base_url . '?access_token=' . urlencode($access_token);
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+    curl_setopt($ch, CURLOPT_HEADER, false);
+
+    $response = curl_exec($ch);
     
 }
 
